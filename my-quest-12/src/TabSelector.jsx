a1fc9df02b81fca424aedbedda3f85aeded2ea7e
Guy de Messimy <gdemessimy@gmail.com>
2020-12-02 17:46:44 +0100

    realize quest

---
+++ my-quest-12/src/TabSelector.jsx
@@ -0,0 +1,30 @@
+function TabSelector ({activeId, handleChangeTab}) {
+    
+    return (
+      <div className="TabSelector">
+        <button
+          id="home"
+          onClick={handleChangeTab}
+          className={activeId === 'home' ? 'active' : ''}
+        >
+          Home
+        </button>
+        <button
+          id="about"
+          onClick={handleChangeTab}
+          className={activeId === 'about' ? 'active' : ''}
+        >
+          About
+        </button>
+        <button
+          id="contact"
+          onClick={handleChangeTab}
+          className={activeId === 'contact' ? 'active' : ''}
+        >
+          Contact
+        </button>
+      </div>
+    );
+  }
+
+export default TabSelector;
